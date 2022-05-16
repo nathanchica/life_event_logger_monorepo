eb5afeeb5b50cd021d2e04b18c47a6eaadd704aa
Nathan Chica <chicanathan@gmail.com>
2022-05-15 18:40:55 -0700

    add lint-staged

---
+++ lint-staged.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    '*.{js,jsx,ts,tsx}': [
+        'eslint --max-warnings=0',
+        () => 'tsc-files --noEmit',
+    ],
+    '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
+}
