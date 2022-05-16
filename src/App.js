88b919294df8735923231568d519d068e180ab16
Nathan Chica <chicanathan@gmail.com>
2022-05-15 18:45:22 -0700

    prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-import logo from './logo.svg'
-import './App.css'
+import logo from "./logo.svg";
+import "./App.css";
 
-function App () {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,7 +21,7 @@ function App () {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
