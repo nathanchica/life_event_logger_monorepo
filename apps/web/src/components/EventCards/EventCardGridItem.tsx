b33659f380c80084cd3dd3f3d6f6277c01ee1d28
Nathan <chicanathan@gmail.com>
2025-08-08 20:24:13 -0700

    forward eventcard refs down to underlying mui grid component

--- apps/web/src/components/EventCards/EventCardGridItem.tsx
+++ apps/web/src/components/EventCards/EventCardGridItem.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, forwardRef } from 'react';
 
 import Grid from '@mui/material/Grid';
 
@@ -13,12 +13,16 @@ type Props = {
 
 /**
  * EventCardGridItem component that provides a consistent Grid item wrapper for event cards.
+ *
+ * Passing ref to underlying MUI Grid component for use with MUI ClickAwayListener
+ * https://mui.com/material-ui/guides/composition/#caveat-with-refs
  */
-const EventCardGridItem = ({ children }: Props) => {
+const EventCardGridItem = forwardRef<HTMLDivElement, Props>(({ children }, ref) => {
     const { isMobile } = useMuiState();
 
     return (
         <Grid
+            ref={ref}
             role="listitem"
             size={{ xs: 12, sm: 6, md: 4, lg: 4 }}
             sx={{
@@ -29,6 +33,6 @@ const EventCardGridItem = ({ children }: Props) => {
             {children}
         </Grid>
     );
-};
+});
 
 export default EventCardGridItem;
