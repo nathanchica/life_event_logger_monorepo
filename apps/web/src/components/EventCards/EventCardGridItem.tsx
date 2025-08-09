30596f63c8cef9148bcfd2d72ad74ea9be85458c
Nathan <chicanathan@gmail.com>
2025-08-08 19:27:22 -0700

    card size mobile regression fix

--- apps/web/src/components/EventCards/EventCardGridItem.tsx
+++ apps/web/src/components/EventCards/EventCardGridItem.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 
 import Grid from '@mui/material/Grid';
 
+import useMuiState from '../../hooks/useMuiState';
+
 const DESKTOP_CARD_MAX_WIDTH = '400px';
 const CARD_MIN_WIDTH = '280px';
 
@@ -13,11 +15,16 @@ type Props = {
  * EventCardGridItem component that provides a consistent Grid item wrapper for event cards.
  */
 const EventCardGridItem = ({ children }: Props) => {
+    const { isMobile } = useMuiState();
+
     return (
         <Grid
             role="listitem"
             size={{ xs: 12, sm: 6, md: 4, lg: 4 }}
-            sx={{ maxWidth: DESKTOP_CARD_MAX_WIDTH, minWidth: CARD_MIN_WIDTH }}
+            sx={{
+                maxWidth: DESKTOP_CARD_MAX_WIDTH,
+                ...(!isMobile && { minWidth: CARD_MIN_WIDTH })
+            }}
         >
             {children}
         </Grid>
